b88925261677f244ca38e88c8e96d6c0de7dcf26
root <root@master.test.com>
2012-10-31 08:38:24 +0800

    bashshell

--- work/rlogin_ssh.sh
+++ work/rlogin_ssh.sh
@@ -1,52 +1,49 @@
 #!/bin/bash
 #Filename:login_ssh.sh
-set -x
+#set -x
 if [ -z $1 ];then
-	echo "Sorry!you must enter the hostname or ip!"
+	echo "--------------------------------------------"
+	echo "+ Sorry!    you must enter the ip!         +"
+	echo "--------------------------------------------"
 	exit
 fi
+
+IP_PASS=`grep $1' ' /tmp/test.txt`
+IP=`echo $IP_PASS | cut -d' ' -f 1`
+U_PASS=`echo $IP_PASS | cut -d' ' -f 3`
+R_PASS=`echo $IP_PASS | cut -d' ' -f 4`
+
 rlogin_root(){
-        expect -c"spawn ssh root@$IP
-                  expect \"*yes/no)?\"
-                  send \"yes\"
-                  expect \"*password:\"
-                  send \"$R_PASS\n\"
-                  interact"
+        expect -c "spawn ssh root@$IP
+		expect \"*yes/no)?\"
+		send \"yes\r\"
+                expect \"*password:\"
+                send \"$R_PASS\r\"
+                interact"
         }
+
 rlogin_user(){
         expect -c"spawn ssh $USER@$IP
-                  expect \"*yes/no)\"
-                  send \"yes\r\"
                   expect \"*password:\"
                   send \"$U_PASS\r\"
                   interact"
         }
-while read IP USER U_PASS R_PASS
-	do
-	if [ $1 == $IP ];then
-		if [ $UID -eq 0 ];then
-			rlogin_root
-#expect -c "spawn ssh root@$IP"
-#expect -c "expect \"*password:\""
-#expect -c "send \"$R_PASS\""
-#expect -c "interact"
 
+if [ $1 == "$IP" ];then
+	if [ $UID -eq 0 ];then
+		rlogin_root
 		sleep 2
-		else
-			rlogin_user
-		fi
+		exit
 	else
-		echo "$1 is not exist!"
+		rlogin_user
 	fi
+else
+	echo "------------------------"
+	echo "+ $1 is not exist!     +"
+	echo "------------------------"
+fi
 
-done</tmp/test.txt
-
-
-
-
-
-
-
+exit
 
 
 
@@ -56,44 +53,3 @@ done</tmp/test.txt
 
 
 
-
-
-
-
-
-#if [ -z $1 ];then
-#        echo "Sorry! you did not enter the hostname!"
-#        exit
-#else
-#`grep $1 /tmp/test.txt > /tmp/rlogin_ssh.txt`
-#fi
-#if [ -z /tmp/rlogin_ssh.txt ];then
-#        echo "Sorry! The $1 is not exist"
-#        exit
-#else
-#	
-#fi
-#   login_root(){
-#        expect -c"spawn ssh root@$IP
-#                expect \"*yes/no\"
-#                send \"yes\r\"
-#                expect \"*password\"
-#                send \"$R_PASS\r\"
-#                interact"
-#        }
-#      login_user(){
-#        expect -c"spawn ssh $USER@$IP
-#                expect \"*yes/no\"
-#                send \"yes\r\"
-#                expect \"*password\"
-#                send \"$U_PASS\r\"
-#                interact"
-#        }
-#
-#if [ $UID -eq 0 ];then
-#        login_root
-#else
-#        login_user
-#fi
-#`rm -rf /tmp/rlogin_ssh.txt `
-#exit
